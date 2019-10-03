9ca679f38dcce1f05536e84c681a068da61af193
Mike <michael.desson@gmail.com>
2019-10-03 14:34:42 -0400

    Created map file and tweaked map header file

--- src/Map.h
+++ src/Map.h
@@ -4,16 +4,8 @@
 #include <utility>
 #include "Map.h"
 
-// Class: Map
-// Node: Struct (refactor to inner classl later?)
-// Store in a map, with a unique id as the key
-// Constructor: add them, then set connectedTo?
-// The map file I'll be given NEEDS a uid, so use whatever that is
-// ^ Otherwise it couldn't do it itself either
-
-// TODO: Find a way to make this private
 // TODO: Make this into a subclass with pointers and a constructor.
-struct Node {
+class Node {
     // region name
     std::string region;
     // continent name
@@ -31,16 +23,38 @@ struct Node {
 
 class Map {
     private:
-        std::map<Node, int[]>* nodes;
+        class Node {
+        // region name
+        std::string* region;
+        // continent name
+        std::string* continent;
+        // key is playername
+        // value is array of two ints: armies, cities
+        std::map<std::string, std::pair<int, int>>* armies;
+        // playername for owner
+        std::string* owner;
+        // array of regions it is connected to
+        // first of pair is region name
+        // second is true if water-crossing
+        std::pair<std::string, bool>* connectedTo[];
 
+        Node(std::string r, std::string c, std::string players[], int armies[][2]);
+        ~Node();
+        };
+
+        // connected graph of Nodes
+        std::map<Node, int[]>* nodes;
+        // starting node
+        std::string* start;
         // returns a node. Helper function for the public methods
         Node getNode(std::string region);
     public:
         // TODO: parameterized constructors
         Map();
+        ~Map();
         // calculate and set the owner for a node
         void updateOwnership(); // update all
-        void updateOwnership(Node n); // update one
+        void updateOwnership(std::string region); // update one
         // add army to Node
         void addArmies(std::string player, std::string region, int armies);
         // remove armies from Node
@@ -49,6 +63,4 @@ class Map {
         void addCity(std::string player, std::string region);
         // remove cities from Node
         void removeCity(std::string player, std::string region);
-        
-        // TODO: Destructor
 };
