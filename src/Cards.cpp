5759ee7369bc446c21fee45bcc1dd3cadf9ae267
cookfisher <cookfisher@outlook.com>
2019-10-11 01:50:01 -0400

    Cards, Deck, Hand skeleton

--- src/Cards.cpp
+++ src/Cards.cpp
@@ -2,28 +2,15 @@
 #include "Cards.h"
 
 
+Cards::Cards() {
+
+}
+
+Cards::Cards(string *good, string *action) : good(good), action(action) {}
+
 Cards::~Cards() {
 
 }
-
-Good *Cards::getGood() const {
-    return good;
-}
-
-void Cards::setGood(Good *good) {
-    Cards::good = good;
-}
-
-Action *Cards::getAction() const {
-    return action;
-}
-
-void Cards::setAction(Action *action) {
-    Cards::action = action;
-}
-
-Cards::Cards(Good *good, Action *action) : good(good), action(action) {}
-
 
 Deck::Deck() {}
 
@@ -46,30 +33,6 @@ void Hand::exchange() {
 }
 
 
-Good::Good() {}
 
-Good::~Good() {
-
-}
-
-Good::Good(int *numOfGood, TypeOfGood *typeOfGood, int *points) : numOfGood(numOfGood), typeOfGood(typeOfGood), points(points) {}
-
-std::ostream &operator<<(std::ostream &os, const Good &good) {
-    os << "numOfGood: " << good.numOfGood << " points: " << good.points << " typeOfGood: " << good.typeOfGood;
-    return os;
-}
-
-Action::Action() {}
-
-Action::~Action() {
-
-}
-
-std::ostream &operator<<(std::ostream &os, const Action &action) {
-    os << "numOfMoves: " << action.numOfMoves << " instruction: " << action.instruction;
-    return os;
-}
-
-Action::Action(int *numOfMoves, string *instruction) : numOfMoves(numOfMoves), instruction(instruction) {}
 
 
--- src/Cards.h
+++ src/Cards.h
@@ -9,108 +9,82 @@
 
 using std::string;
 
-class Good {
-
-private:
-    int *numOfGood, *points;
-    enum TypeOfGood {TREE, CARROT, ANVIL, CRYSTAL, ROCK};
-    TypeOfGood *typeOfGood;
-public:
-    Good();
-
-    Good(int *numOfGood, TypeOfGood *typeOfGood, int *points);
-
-    virtual ~Good();
-
-    inline int *getNumOfGood() const {
-        return numOfGood;
-    }
-
-    inline void setNumOfGood(int *numOfGood) {
-        Good::numOfGood = numOfGood;
-    }
-
-    inline int *getPoints() const {
-        return points;
-    }
-
-    inline void setPoints(int *points) {
-        Good::points = points;
-    }
-
-    inline TypeOfGood *getTypeOfGood() const {
-        return typeOfGood;
-    }
-
-    inline void setTypeOfGood(TypeOfGood *typeOfGood) {
-        Good::typeOfGood = typeOfGood;
-    }
-
-    friend std::ostream &operator<<(std::ostream &os, const Good &good);
-
-    friend class Cards;
-};
-
-class Action {
-
-private:
-    int *numOfMoves;
-    string *instruction;
-//    enum Instruction { PLACE_NEW_ARMIES_ON_BOARD, MOVE_ARMIES, MOVE_OVER_LAND, MOVE_OVER_WATER, BUILD_A_CITY, DESTROY_ARMY, AND, OR };
-public:
-    Action();
-
-    Action(int *numOfMoves, string *instruction);
-
-    virtual ~Action();
-
-    inline int *getNumOfMoves() const {
-        return numOfMoves;
-    }
-
-    inline void setNumOfMoves(int *numOfMoves) {
-        Action::numOfMoves = numOfMoves;
-    }
-
-    inline string *getInstruction() const {
-        return instruction;
-    }
-
-    inline void setInstruction(string *instruction) {
-        Action::instruction = instruction;
-    }
-
-    friend std::ostream &operator<<(std::ostream &os, const Action &action);
-
-    friend class Cards;
-
-};
-
 class Cards {
 private:
-    Good *good;
-    Action *action;
+    string *good;
+    string *action;
 public:
-    Cards();
 
-    Cards(Good *good, Action *action);
-
+    Cards(string *good, string *action);
     virtual ~Cards();
 
-    Good *getGood() const;
+    inline string *getGood() const {
+        return good;
+    }
 
-    void setGood(Good *good);
+    inline void setGood(string *good) {
+        Cards::good = good;
+    }
 
-    Action *getAction() const;
+    inline string *getAction() const {
+        return action;
+    }
 
-    void setAction(Action *action);
+    inline void setAction(string *action) {
+        Cards::action = action;
+    }
+
 };
 
 class Deck {
-    Cards cards;
     std::vector<Cards> deck;
 public:
+    const Cards cards01 = Cards((string *) "Wild", (string *)"MOVE_OVER_WATER 2");
+    const Cards cards02 = Cards((string *) "Wild", (string *)"MOVE_OVER_WATER 2");
+    const Cards cards03 = Cards((string *) "Wild", (string *)"PLACE_NEW_ARMIES_ON_BOARD 2");
+    const Cards cards04 = Cards((string *) "Diamond", (string *)"PLACE_NEW_ARMIES_ON_BOARD 2");
+    const Cards cards05 = Cards((string *) "Diamond", (string *)"PLACE_NEW_ARMIES_ON_BOARD 2");
+    const Cards cards06 = Cards((string *) "Diamond", (string *)"PLACE_NEW_ARMIES_ON_BOARD 2");
+    const Cards cards07 = Cards((string *) "Diamond", (string *)"PLACE_NEW_ARMIES_ON_BOARD 1");
+    const Cards cards08 = Cards((string *) "Diamond", (string *)"MOVE_OVER_LAND 2");
+    const Cards cards09 = Cards((string *) "Coal", (string *)"MOVE_OVER_LAND 2");
+    const Cards cards10 = Cards((string *) "Coal", (string *)"MOVE_OVER_WATER 2");
+    const Cards cards11 = Cards((string *) "Coal", (string *)"MOVE_OVER_WATER 2");
+    const Cards cards12 = Cards((string *) "Coal", (string *)"MOVE_OVER_WATER 3");
+    const Cards cards13 = Cards((string *) "Coal", (string *)"PLACE_NEW_ARMIES_ON_BOARD 3");
+    const Cards cards14 = Cards((string *) "Coal", (string *)"PLACE_NEW_ARMIES_ON_BOARD 3");
+    const Cards cards15 = Cards((string *) "Coal", (string *)"PLACE_NEW_ARMIES_ON_BOARD 2 OR BUILD_A_CITY");
+    const Cards cards16 = Cards((string *) "Carrot", (string *)"BUILD_A_CITY");
+    const Cards cards17 = Cards((string *) "Carrot", (string *)"DESTROY_ARMY 1 AND PLACE_NEW_ARMIES_ON_BOARD 1");
+    const Cards cards18 = Cards((string *) "Carrot", (string *)"PLACE_NEW_ARMIES_ON_BOARD 3");
+    const Cards cards19 = Cards((string *) "Carrot", (string *)"MOVE_OVER_LAND 4");
+    const Cards cards20 = Cards((string *) "Carrot", (string *)"MOVE_OVER_LAND 4");
+    const Cards cards21 = Cards((string *) "Carrot", (string *)"PLACE_NEW_ARMIES_ON_BOARD 4 OR MOVE_OVER_LAND 2");
+    const Cards cards22 = Cards((string *) "Carrot", (string *)"MOVE_OVER_LAND 5");
+    const Cards cards23 = Cards((string *) "Carrot", (string *)"BUILD_A_CITY");
+    const Cards cards24 = Cards((string *) "Carrot", (string *)"MOVE_OVER_WATER 3");
+    const Cards cards25 = Cards((string *) "Carrot 2", (string *)"PLACE_NEW_ARMIES_ON_BOARD 3");
+    const Cards cards26 = Cards((string *) "Tree", (string *)"MOVE_OVER_LAND 5");
+    const Cards cards27 = Cards((string *) "Tree", (string *)"MOVE_OVER_LAND 6");
+    const Cards cards28 = Cards((string *) "Tree", (string *)"PLACE_NEW_ARMIES_ON_BOARD 2 OR MOVE_OVER_LAND 3");
+    const Cards cards29 = Cards((string *) "Tree", (string *)"DESTROY_ARMY 1 OR BUILD_A_CITY");
+    const Cards cards30 = Cards((string *) "Tree", (string *)"PLACE_NEW_ARMIES_ON_BOARD 3");
+    const Cards cards31 = Cards((string *) "Tree", (string *)"MOVE_OVER_WATER 4");
+    const Cards cards32 = Cards((string *) "Tree", (string *)"MOVE_OVER_WATER 3");
+    const Cards cards33 = Cards((string *) "Tree", (string *)"BUILD_A_CITY");
+    const Cards cards34 = Cards((string *) "Anvil", (string *)"PLACE_NEW_ARMIES_ON_BOARD 3");
+    const Cards cards35 = Cards((string *) "Anvil", (string *)"PLACE_NEW_ARMIES_ON_BOARD 3");
+    const Cards cards36 = Cards((string *) "Anvil", (string *)"MOVE_OVER_WATER 3");
+    const Cards cards37 = Cards((string *) "Anvil", (string *)"BUILD_A_CITY");
+    const Cards cards38 = Cards((string *) "Anvil 2", (string *)"MOVE_OVER_LAND 4");
+    const Cards cards39 = Cards((string *) "Anvil", (string *)"MOVE_OVER_LAND 5");
+    const Cards cards40 = Cards((string *) "Anvil", (string *)"MOVE_OVER_LAND 4");
+    const Cards cards41 = Cards((string *) "Anvil", (string *)"PLACE_NEW_ARMIES_ON_BOARD 3 OR MOVE_OVER_LAND 4");
+    const Cards cards42 = Cards((string *) "Anvil", (string *)"PLACE_NEW_ARMIES_ON_BOARD 3 OR MOVE_OVER_LAND 3");
 
+
+//    enum TypeOfGood {TREE, CARROT, ANVIL, CRYSTAL, ROCK};
+//    enum Instruction { PLACE_NEW_ARMIES_ON_BOARD, MOVE_ARMIES, MOVE_OVER_LAND, MOVE_OVER_WATER, BUILD_A_CITY, DESTROY_ARMY, AND, OR };
     Deck();
 
     virtual ~Deck();
