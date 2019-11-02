e07d25d033d07ce2920877bd6caf1a93f1d71762
mdesson <michael.desson@gmail.com>
2019-11-02 11:39:08 -0400

    Added hand of cards to Player

--- src/Player/Player.cpp
+++ src/Player/Player.cpp
@@ -1,136 +1,129 @@
+#include "Player.h"
+#include "../BiddingFacility/BiddingFacility.h"
+#include "../Map/Map.h"
 #include <iostream>
 #include <string>
-#include "Player.h"
-#include "../Map/Map.h"
-#include "../BiddingFacility/BiddingFacility.h"
 
-//Constructors
+// Constructors
 
 Player::Player(std::string name, int dateOfBirth) {
-    Player::cities = new int(3);
-    Player::armies = new int(14);
-    Player::coins = new int(14);
-    Player::name = new std::string(name);
-    Player::dateOfBirth = new int(dateOfBirth);
-    Player::biddingFacility = new BiddingFacility();
+  Player::cities = new int(3);
+  Player::armies = new int(14);
+  Player::coins = new int(14);
+  Player::name = new std::string(name);
+  Player::dateOfBirth = new int(dateOfBirth);
+  Player::biddingFacility = new BiddingFacility();
 }
 
 Player::Player(std::string name, int numOfPlayers, int dateOfBirth) {
-    Player::cities = new int(3);
-    Player::armies = new int(14);
-    Player::name = new std::string(name);
-    Player::dateOfBirth = new int(dateOfBirth);
-    Player::biddingFacility = new BiddingFacility();
+  Player::cities = new int(3);
+  Player::armies = new int(14);
+  Player::name = new std::string(name);
+  Player::dateOfBirth = new int(dateOfBirth);
+  Player::biddingFacility = new BiddingFacility();
 
-    // Amount of coins available to Players is determined by # of Players
-    switch (numOfPlayers) {
-        case 2 :
-            Player::coins = new int(14);
-            break;
-        case 3 :
-            Player::coins = new int(11);
-            break;
-        case 4 :
-            Player::coins = new int(9);
-            break;
-        case 5 :
-            Player::coins = new int(8);
-            break;
-    }
+  // Amount of coins available to Players is determined by # of Players
+  switch (numOfPlayers) {
+  case 2:
+    Player::coins = new int(14);
+    break;
+  case 3:
+    Player::coins = new int(11);
+    break;
+  case 4:
+    Player::coins = new int(9);
+    break;
+  case 5:
+    Player::coins = new int(8);
+    break;
+  }
 }
 
 // Deconstructor
 
 Player::~Player() {
-    delete this->coins;
-    delete this->armies;
-    delete this->cities;
-    delete this->dateOfBirth;
-    delete this->name;
-    delete this->biddingFacility;
+  delete this->coins;
+  delete this->armies;
+  delete this->cities;
+  delete this->dateOfBirth;
+  delete this->name;
+  delete this->biddingFacility;
 };
 
 // Gameplay Methods
 
 bool Player::payCoin(int cost) {
-    if (*(this->getCoins()) < cost) {
-        std::cout << "You don't have enough coins to purchase that." << std::endl;
-        return false;
-    } else {
-        this->setCoins(*(this->getCoins()) - cost);
-        std::cout << "Successful Purchase, you have " << *(this->getCoins()) << " amount of coins remaining" << std::endl;
-        return true;
-    }
+  if (*(this->getCoins()) < cost) {
+    std::cout << "You don't have enough coins to purchase that." << std::endl;
+    return false;
+  } else {
+    this->setCoins(*(this->getCoins()) - cost);
+    std::cout << "Successful Purchase, you have " << *(this->getCoins())
+              << " amount of coins remaining" << std::endl;
+    return true;
+  }
 }
 
-bool Player::placeNewArmies(int totalArmies, std::string regionName, Map gameBoard) {
-    return gameBoard.addArmy(regionName, *this->getName());
+bool Player::placeNewArmies(int totalArmies, std::string regionName,
+                            Map gameBoard) {
+  return gameBoard.addArmy(regionName, *this->getName());
 }
 
 // TODO: Assignment 2
 void Player::moveOverWater(int totalMoves) {
-    std::cout << "This method will allow the player to move " << totalMoves << " armies across water." << std::endl;
+  std::cout << "This method will allow the player to move " << totalMoves
+            << " armies across water." << std::endl;
 }
 
 // TODO: Assignment 2
 void Player::moveOverLand(int totalMoves) {
-    std::cout << "This method will allow the player to move " << totalMoves << " armies across land." << std::endl;
+  std::cout << "This method will allow the player to move " << totalMoves
+            << " armies across land." << std::endl;
 }
 
 // TODO: Assignment 2
 void Player::buildCity() {
-    std::cout << "This method will allow the player to build 1 city." << std::endl;
+  std::cout << "This method will allow the player to build 1 city."
+            << std::endl;
 }
 
 // TODO: Assignment 2
 void Player::destroyArmy() {
-    std::cout << "This method will allow the player to destroy other player's armies." << std::endl;
+  std::cout
+      << "This method will allow the player to destroy other player's armies."
+      << std::endl;
 }
 
 // Accessors
 
-int* Player::getCoins() const {
-    return coins;
-}
+int *Player::getCoins() const { return coins; }
 
-int* Player::getArmies() const {
-    return armies;
-}
+int *Player::getArmies() const { return armies; }
 
-int* Player::getCities() const {
-    return cities;
-}
+int *Player::getCities() const { return cities; }
 
-int *Player::getDateOfBirth() const {
-    return dateOfBirth;
-}
+int *Player::getDateOfBirth() const { return dateOfBirth; }
 
-std::string *Player::getName() const {
-    return name;
-}
+std::string *Player::getName() const { return name; }
 
-BiddingFacility *Player::getBiddingFacility() const {
-    return biddingFacility;
-}
+std::vector<Cards> *Player::getGameHand() const { return gameHand; }
+
+BiddingFacility *Player::getBiddingFacility() const { return biddingFacility; }
 
 // Mutators
 
-void Player::setCoins(int coins) {
-    *Player::coins = coins;
-}
+void Player::setCoins(int coins) { *Player::coins = coins; }
 
-void Player::setArmies(int armies) {
-    *Player::armies = armies;
-}
+void Player::setArmies(int armies) { *Player::armies = armies; }
 
-void Player::setCities(int cities) {
-    *Player::cities = cities;
-}
+void Player::setCities(int cities) { *Player::cities = cities; }
 
 void Player::setDateOfBirth(int dateOfBirth) {
-    *Player::dateOfBirth = dateOfBirth;
+  *Player::dateOfBirth = dateOfBirth;
 }
 
-void Player::setName(std::string name) {
-    *Player::name = name;
+void Player::setName(std::string name) { *Player::name = name; }
+
+void Player::addCards(Cards card) {
+  gameHand->emplace_back(Cards(card.getGood(), card.getAction()));
 }
--- src/Player/Player.h
+++ src/Player/Player.h
@@ -1,11 +1,11 @@
 #ifndef EIGHT_MINUTE_EMPIRE_PLAYER_H
 #define EIGHT_MINUTE_EMPIRE_PLAYER_H
 
+#include "../BiddingFacility/BiddingFacility.h"
+#include "../Cards/Cards.h"
+#include "../Map/Map.h"
 #include <string>
 #include <vector>
-#include "../BiddingFacility/BiddingFacility.h"
-#include "../Map/Map.h"
-#include "../Cards/Cards.h"
 
 // Forward Declaration
 class BiddingFacility;
@@ -13,46 +13,48 @@ class BiddingFacility;
 class Player {
 
 private:
-    int* coins;
-    int* armies;
-    int* cities;
-    int* dateOfBirth;
-    std::string* name;
-    std::vector<std::string>* regions;          // TODO: Figure out why we need this
-    std::vector<std::string>* countries;        // TODO: Figure out why we need this
-    std::vector<Deck>* gameHand;
-    BiddingFacility* biddingFacility;
+  int *coins;
+  int *armies;
+  int *cities;
+  int *dateOfBirth;
+  std::string *name;
+  std::vector<std::string> *regions;   // TODO: Figure out why we need this
+  std::vector<std::string> *countries; // TODO: Figure out why we need this
+  std::vector<Cards> *gameHand;
+  BiddingFacility *biddingFacility;
 
 public:
-    // Constructors
-    Player(std::string name, int dateOfBirth);
-    Player(std::string name, int numOfPlayers, int dateOfBirth);
+  // Constructors
+  Player(std::string name, int dateOfBirth);
+  Player(std::string name, int numOfPlayers, int dateOfBirth);
 
-    // Deconstructor
-    ~Player();
+  // Deconstructor
+  ~Player();
 
-    // Gameplay Methods
-    bool payCoin(int cost);
-    bool placeNewArmies(int totalArmies, std::string regionName, Map gameMap);
-    void moveOverWater(int totalMoves);     // TODO: Fully implement in assignment 2
-    void moveOverLand(int totalMoves);      // TODO: Fully implement in assignment 2
-    void buildCity();                       // TODO: Fully implement in assignment 2
-    void destroyArmy();                     // TODO: Fully implement in assignment 2
+  // Gameplay Methods
+  bool payCoin(int cost);
+  bool placeNewArmies(int totalArmies, std::string regionName, Map gameMap);
+  void moveOverWater(int totalMoves); // TODO: Fully implement in assignment 2
+  void moveOverLand(int totalMoves);  // TODO: Fully implement in assignment 2
+  void buildCity();                   // TODO: Fully implement in assignment 2
+  void destroyArmy();                 // TODO: Fully implement in assignment 2
 
-    // Accessors
-    int* getCoins() const;
-    int* getArmies() const;
-    int* getCities() const;
-    int* getDateOfBirth() const;
-    std::string* getName() const;
-    BiddingFacility* getBiddingFacility() const;
+  // Accessors
+  int *getCoins() const;
+  int *getArmies() const;
+  int *getCities() const;
+  int *getDateOfBirth() const;
+  std::string *getName() const;
+  std::vector<Cards> *getGameHand() const;
+  BiddingFacility *getBiddingFacility() const;
 
-    // Mutators
-    void setCoins(int coins);
-    void setArmies(int armies);
-    void setCities(int cities);
-    void setDateOfBirth(int dateOfBirth);
-    void setName(std::string name);
+  // Mutators
+  void setCoins(int coins);
+  void setArmies(int armies);
+  void setCities(int cities);
+  void setDateOfBirth(int dateOfBirth);
+  void setName(std::string name);
+  void addCards(Cards card);
 };
 
 #endif
