4f79ed7e0ef0d5178fd617d9735afe90e5ce50ac
cookfisher <cookfisher@outlook.com>
2019-11-04 18:10:57 -0500

    before pull

--- src/AfterAction/AfterActionDriver.cpp
+++ src/AfterAction/AfterActionDriver.cpp
@@ -12,23 +12,23 @@ void afterActionExample() {
 //    deck100.draw();
 
 
-    std::cout<< "======== Test Top Board and Play pass to the next ====== "<< endl;
-    Deck deck1;
-    std::vector<Cards*> tb = deck1.topBoardGenetor(deck1);
-    std::vector<Cards*> *hand1 = new std::vector<Cards*>();
-    std::vector<Cards*> *hand2 = new std::vector<Cards*>();
-    std::vector<Cards*> *hand3 = new std::vector<Cards*>();
-    for (int i = 1; i < 4; i++) {
-        deck1.exchange(*hand1, tb, deck1);
-        deck1.exchange(*hand2, tb, deck1);
-        deck1.exchange(*hand3, tb, deck1);
-    }
-    deck1.displayTopBoard(tb);
-    cout << "============= Player 1 =================" << endl;
-    deck1.displayHand(*hand1);
-    cout << "============= Player 2 =================" << endl;
-    deck1.displayHand(*hand2);
-    cout << "============= Player 3 =================" << endl;
-    deck1.displayHand(*hand3);
+//    std::cout<< "======== Test Top Board and Play pass to the next ====== "<< endl;
+//    Deck deck1;
+//    std::vector<Cards*> tb = deck1.topBoardGenetor(deck1);
+//    std::vector<Cards*> *hand1 = new std::vector<Cards*>();
+//    std::vector<Cards*> *hand2 = new std::vector<Cards*>();
+//    std::vector<Cards*> *hand3 = new std::vector<Cards*>();
+//    for (int i = 1; i < 4; i++) {
+//        deck1.exchange(*hand1, tb, deck1);
+//        deck1.exchange(*hand2, tb, deck1);
+//        deck1.exchange(*hand3, tb, deck1);
+//    }
+//    deck1.displayTopBoard(tb);
+//    cout << "============= Player 1 =================" << endl;
+//    deck1.displayHand(*hand1);
+//    cout << "============= Player 2 =================" << endl;
+//    deck1.displayHand(*hand2);
+//    cout << "============= Player 3 =================" << endl;
+//    deck1.displayHand(*hand3);
 
 }
--- src/GameScore/GameScoreDriver.cpp
+++ src/GameScore/GameScoreDriver.cpp
@@ -8,20 +8,20 @@
 void gameScoreExample() {
 
     cout << "=========Build a Player's Hand ===============" << endl;
-    Deck deck1;
-    std::vector<Cards*> tb = deck1.topBoardGenetor(deck1);
-    std::vector<Cards*> *hand = new std::vector<Cards*>();
-    deck1.exchange(*hand, tb, deck1);
-    deck1.exchange(*hand, tb, deck1);
-    deck1.exchange(*hand, tb, deck1);
-    deck1.exchange(*hand, tb, deck1);
-    deck1.exchange(*hand, tb, deck1);
-    deck1.exchange(*hand, tb, deck1);
-    deck1.displayTopBoard(tb);
-    deck1.displayHand(*hand);
+//    Deck deck1;
+//    std::vector<Cards*> tb = deck1.topBoardGenetor(deck1);
+//    std::vector<Cards*> *hand = new std::vector<Cards*>();
+//    deck1.exchange(*hand, tb, deck1);
+//    deck1.exchange(*hand, tb, deck1);
+//    deck1.exchange(*hand, tb, deck1);
+//    deck1.exchange(*hand, tb, deck1);
+//    deck1.exchange(*hand, tb, deck1);
+//    deck1.exchange(*hand, tb, deck1);
+//    deck1.displayTopBoard(tb);
+//    deck1.displayHand(*hand);
     cout << "=========Test GameScore( ) ===============" << endl;
-    std::vector<Player*> *players = new vector<Player*> ();
-    GameScore gameScore;
-    int* sum = gameScore.computeGameScore(*players, *hand);
-    cout << "Game Score for player is " << *sum << " points" << endl;
+//    std::vector<Player*> *players = new vector<Player*> ();
+//    GameScore gameScore;
+//    int* sum = gameScore.computeGameScore(*players, *hand);
+//    cout << "Game Score for player is " << *sum << " points" << endl;
 }
