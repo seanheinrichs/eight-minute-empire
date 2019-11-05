c20ef2fa40fca96dc16fcc664375c1559aef7265
cookfisher <cookfisher@outlook.com>
2019-11-05 00:05:33 -0500

    finish half

---
+++ src/GameScore/GameScore.cpp
@@ -0,0 +1,264 @@
+
+#include "GameScore.h"
+#include "Player.h"
+#include "Cards.h"
+#include "GameState.h"
+#include "Map.h"
+
+GameScore::GameScore() {}
+
+GameScore::~GameScore() {
+
+}
+
+GameScore::GameScore(const vector<Cards *> &gameHand, const vector<Player *> &players) : gameHand(gameHand),
+                                                                                         players(players) {}
+
+
+int *GameScore::countTreePoint(int *tree) {
+    switch (*tree)
+    {
+        case 2:
+            point = new int(1);
+            break;
+        case 4:
+            point = new int(2);
+            break;
+        case 5:
+            point = new int(3);
+            break;
+        case 6:
+            point = new int(5);
+            break;
+    }
+    return point;
+}
+
+int *GameScore::countAnvilPoint(int *anvil) {
+    switch (*anvil)
+    {
+        case 2:
+            point = new int(1);
+            break;
+        case 4:
+            point = new int(2);
+            break;
+        case 6:
+            point = new int(3);
+            break;
+        case 7:
+            point = new int(5);
+            break;
+    }
+    return point;
+}
+
+int *GameScore::countCarrotPoint(int *carrot) {
+    switch (*carrot)
+    {
+        case 3:
+            point = new int(1);
+            break;
+        case 5:
+            point = new int(2);
+            break;
+        case 7:
+            point = new int(3);
+            break;
+        case 8:
+            point = new int(5);
+            break;
+    }
+    return point;
+}
+
+int *GameScore::countRockPoint(int *rock) {
+    switch (*rock)
+    {
+        case 2:
+            point = new int(1);
+            break;
+        case 3:
+            point = new int(2);
+            break;
+        case 4:
+            point = new int(3);
+            break;
+        case 5:
+            point = new int(5);
+            break;
+    }
+    return point;
+}
+
+int *GameScore::countCrystalPoint(int *crystal) {
+    switch (*crystal)
+    {
+        case 1:
+            point = new int(1);
+            break;
+        case 2:
+            point = new int(2);
+            break;
+        case 3:
+            point = new int(3);
+            break;
+        case 4:
+            point = new int(5);
+            break;
+    }
+    return point;
+}
+
+
+int *GameScore::computeGameScore(Player * &players, std::vector<Cards *> &gameHand) {
+    int* sum = new int(0);
+    int* rock = new int(0);
+    int* crystal = new int(0);
+    int* anvil = new int(0);
+    int* wild = new int(0);
+    int* carrot = new int(0);
+    int* tree = new int(0);
+
+    int* numTree = numOfTree(gameHand);
+    int* numRock = numOfRock(gameHand);
+    int* numCrystal = numOfCrystal(gameHand);
+    int* numCarrot = numOfCarrot(gameHand);
+    int* numAnvil = numOfAnvil(gameHand);
+    int* numWild = numOfWild(gameHand);
+
+    // check if player's hand has Wild cards
+    if (*numWild > 0) {
+        std::cout << "You have wild: " << *numWild << ", rock: " << *numRock <<
+                  ", tree: " << *numTree << ", crystal: " << *numCrystal << ", carrot: " << *numCarrot
+                  << ", anvil card: " << *numAnvil << endl;
+        for (auto i = 0; i < *numWild; i++) {
+            std::string input;
+
+            std::cout << "You have wild cards which can exchange to other cards if you hold at least one card of the same good type." << endl;
+            std::cout << "Please note if you input the one that actually you don't have that type of good card, your wild card will be forfeit : )" << endl;
+            std::cout << "Which card would you like to exchange? Please input in lowercase (Eg. rock tree crystal carrot anvil)" << endl;
+            std::cin >> input;
+            if (*numRock >0  && input.compare("rock") == 0)
+                *numRock++;
+            else if (*numTree > 0 && input.compare("tree") == 0)
+                *numTree++;
+            else if (*numCrystal > 0 && input.compare("crystal") == 0)
+                *numCrystal++;
+            else if (*numCarrot > 0 && input.compare("carrot") == 0)
+                *numCarrot++;
+            else if (*numAnvil > 0 && input.compare("anvil") == 0)
+                *numAnvil++;
+            else
+                continue;
+        }
+    }
+    rock = countRockPoint(numRock);
+    crystal = countCrystalPoint(numCrystal);
+    anvil = countAnvilPoint(numAnvil);
+    carrot = countCarrotPoint(carrot);
+    tree = countTreePoint(tree);
+    *sum = *rock + *crystal + *anvil + *carrot + *tree;
+    return sum;
+}
+
+int *GameScore::numOfTree(std::vector<Cards*> &gameHand) {
+    int* num = new int(0); int* sum = new int(0);
+    for (int i = 0; i < gameHand.size(); i++) {
+         if (gameHand[i]->getGood()->compare("Tree") == 0) {
+            num = gameHand[i]->getNumOfGood();
+            *sum += *num;
+         }
+    }
+    return sum;
+}
+
+int *GameScore::numOfAnvil(std::vector<Cards *> &gameHand) {
+    int* num = new int(0); int* sum = new int(0);
+    for (int i = 0; i < gameHand.size(); i++) {
+        if (gameHand[i]->getGood()->compare("Anvil") == 0) {
+            num = gameHand[i]->getNumOfGood();
+            *sum += *num;
+        }
+    }
+    return sum;
+}
+
+int *GameScore::numOfCarrot(std::vector<Cards *> &gameHand) {
+    int* num = new int(0); int* sum = new int(0);
+    for (int i = 0; i < gameHand.size(); i++) {
+        if (gameHand[i]->getGood()->compare("Carrot") == 0) {
+            num = gameHand[i]->getNumOfGood();
+            *sum += *num;
+        }
+    }
+    return sum;
+}
+
+int *GameScore::numOfRock(std::vector<Cards *> &gameHand) {
+    int* num = new int(0); int* sum = new int(0);
+    for (int i = 0; i < gameHand.size(); i++) {
+        if (gameHand[i]->getGood()->compare("Rock") == 0) {
+            num = gameHand[i]->getNumOfGood();
+            *sum += *num;
+        }
+    }
+    return sum;
+}
+
+int *GameScore::numOfCrystal(std::vector<Cards *> &gameHand) {
+    int* num = new int(0); int* sum = new int(0);
+    for (int i = 0; i < gameHand.size(); i++) {
+        if (gameHand[i]->getGood()->compare("Crystal") == 0) {
+            num = gameHand[i]->getNumOfGood();
+            *sum += *num;
+        }
+    }
+    return sum;
+}
+
+int *GameScore::numOfWild(std::vector<Cards *> &gameHand) {
+    int* num = new int(0); int* sum = new int(0);
+    for (int i = 0; i < gameHand.size(); i++) {
+        if (gameHand[i]->getGood()->compare("Wild") == 0) {
+            num = gameHand[i]->getNumOfGood();
+            *sum += *num;
+        }
+    }
+    return sum;
+}
+
+void GameScore::winnerGenerator(std:: vector <Player*> playerVector) {
+    auto mostScorePlayers = new std::vector<Player *>();
+    int *max = new int(0);
+    std::string winner;
+
+    mostScorePlayers->emplace_back(playerVector.at(0));
+    *max = playerVector.at(0)->getPoints();  // max Points
+    // 1st loop get Player vector with most Points, note the max # of players is 5 in this game
+    for (int i = 1; i < playerVector.size(); i++) {
+        // playerVector.at(0) already inside mostScore group
+        if (playerVector.at(i)->getPoints() > *max) {
+            for (int j = 0; j < mostScorePlayers->size(); j++)
+                mostScorePlayers->pop_back();
+            mostScorePlayers->emplace_back(playerVector.at(i));
+            *max = playerVector.at(i)->getPoints();
+        } else if (playerVector.at(i)->getPoints() == *max) {
+            mostScorePlayers->emplace_back(playerVector.at(i));
+        } else
+            continue;
+    }
+    if (mostScorePlayers->size() == 1) {
+        winner = mostScorePlayers->at(0)->getName();
+        std::cout << "Winner is " << winner << endl;
+        return;
+    } else {
+        winner = mostScorePlayers->at(0)->getName();
+        std::cout << "Winner is " << winner << endl;
+        return;
+    }
+}
+
+//    auto numArmiesOnMap = 14 - players.at(0)->getArmies();
+//    auto numCoins = players.at(0)->getCoins();
+//    auto numCities = players.at(0)->getCities();
---
+++ src/GameScore/GameScoreDriver.cpp
@@ -0,0 +1,126 @@
+#pragma once
+
+#include "GameScoreDriver.h"
+#include "GameScore.h"
+#include "Cards.h"
+#include "Player.h"
+#include "Map.h"
+
+void gameScoreExample() {
+    MapLoader loader;
+    std::string fileLocation = "../assets/map1.map";
+    Map m = loader.generateMap(fileLocation);
+    auto regionOwners = m.getRegionOwners();
+    auto continentOwners = m.getContinentOwners();
+
+    Cards* cards01 = new Cards(1,"Rock", "MOVE_OVER_WATER 2");
+    Cards* cards02 = new Cards(1,"Rock", "MOVE_OVER_WATER 2");
+    Cards* cards03 = new Cards(1,"Rock", "PLACE_NEW_ARMIES_ON_BOARD 2");
+    Cards* cards04 = new Cards(1,"Crystal", "PLACE_NEW_ARMIES_ON_BOARD 2");
+    Cards* cards05 = new Cards(1,"Crystal", "PLACE_NEW_ARMIES_ON_BOARD 2");
+    Cards* cards06 = new Cards(1,"Crystal", "PLACE_NEW_ARMIES_ON_BOARD 2");
+    Cards* cards07 = new Cards(1,"Crystal", "PLACE_NEW_ARMIES_ON_BOARD 1");
+    Cards* cards08 = new Cards(1,"Crystal", "MOVE_OVER_LAND 2");
+    Cards* cards09 = new Cards(1,"Rock", "MOVE_OVER_LAND 2");
+    Cards* cards10 = new Cards(1,"Rock", "MOVE_OVER_WATER 2");
+    Cards* cards11 = new Cards(1,"Rock", "MOVE_OVER_WATER 2");
+    Cards* cards12 = new Cards(1, "Rock", "MOVE_OVER_WATER 3");
+    Cards* cards13 = new Cards(1,"Rock", "PLACE_NEW_ARMIES_ON_BOARD 3");
+    Cards* cards14 = new Cards(1,"Rock", "PLACE_NEW_ARMIES_ON_BOARD 3");
+    Cards* cards15 = new Cards(1,"Rock", "PLACE_NEW_ARMIES_ON_BOARD 2 OR BUILD_A_CITY 1");
+    Cards* cards16 = new Cards(1,"Carrot", "BUILD_A_CITY 1");
+    Cards* cards17 = new Cards(1,"Wild", "DESTROY_ARMY 1 AND PLACE_NEW_ARMIES_ON_BOARD 1");
+    Cards* cards18 = new Cards(1,"Carrot", "PLACE_NEW_ARMIES_ON_BOARD 3");
+    Cards* cards19 = new Cards(1,"Carrot", "MOVE_OVER_LAND 4");
+    Cards* cards20 = new Cards(1,"Carrot", "MOVE_OVER_LAND 4");
+    Cards* cards21 = new Cards(1,"Carrot","PLACE_NEW_ARMIES_ON_BOARD 4 OR MOVE_OVER_LAND 2");
+
+    Player *sean = new Player("sean", 3, 15);
+    Player *mike = new Player("mike", 3, 16);
+    Player *jia = new Player("jia", 3, 17);
+    std:: vector <Player*> playerVector = {sean, mike, jia};
+
+    cout << "=========Build a Player's Hand ===============" << endl;
+    playerVector.at(0)->getGameHand()->emplace_back(cards01);
+    playerVector.at(0)->getGameHand()->emplace_back(cards02);
+    playerVector.at(0)->getGameHand()->emplace_back(cards03);
+    playerVector.at(0)->getGameHand()->emplace_back(cards04);
+    playerVector.at(0)->getGameHand()->emplace_back(cards05);
+    playerVector.at(0)->getGameHand()->emplace_back(cards06);
+    playerVector.at(0)->getGameHand()->emplace_back(cards07);
+
+    playerVector.at(1)->getGameHand()->emplace_back(cards08);
+    playerVector.at(1)->getGameHand()->emplace_back(cards09);
+    playerVector.at(1)->getGameHand()->emplace_back(cards10);
+    playerVector.at(1)->getGameHand()->emplace_back(cards11);
+    playerVector.at(1)->getGameHand()->emplace_back(cards12);
+    playerVector.at(1)->getGameHand()->emplace_back(cards13);
+    playerVector.at(1)->getGameHand()->emplace_back(cards14);
+    playerVector.at(1)->getGameHand()->emplace_back(cards15);
+
+    playerVector.at(2)->getGameHand()->emplace_back(cards15);
+    playerVector.at(2)->getGameHand()->emplace_back(cards16);
+    playerVector.at(2)->getGameHand()->emplace_back(cards17);
+    playerVector.at(2)->getGameHand()->emplace_back(cards18);
+    playerVector.at(2)->getGameHand()->emplace_back(cards19);
+    playerVector.at(2)->getGameHand()->emplace_back(cards20);
+    playerVector.at(2)->getGameHand()->emplace_back(cards21);
+
+    playerVector.at(0)->addPoints(regionOwners.at("sean").size());
+    playerVector.at(1)->addPoints(regionOwners.at("mike").size());
+    playerVector.at(2)->addPoints(regionOwners.at("jia").size());
+
+    for (int i=0; i < continentOwners.size(); i++) {
+        if (continentOwners.at(i).second == "sean") {
+            playerVector.at(0)->addPoints(1);
+        } else if (continentOwners.at(i).second == "mike") {
+            playerVector.at(1)->addPoints(1);
+        } else if (continentOwners.at(i).second == "jia") {
+            playerVector.at(2)->addPoints(1);
+        }
+    }
+    std::cout << "sean points: " << playerVector.at(0)->getPoints() << endl;
+    std::cout << "mike points: " << playerVector.at(1)->getPoints() << endl;
+    std::cout << "jia points: " << playerVector.at(2)->getPoints() << endl;
+
+    auto hand1 = playerVector.at(0)->getGameHand();
+    std::vector<Cards*> *hand2 = playerVector.at(1)->getGameHand();
+    std::vector<Cards*> *hand3 = playerVector.at(2)->getGameHand();
+    GameScore gameScore;
+    cout << "=========Test GameScore( ) ===============" << endl;
+    int* sum1 = gameScore.computeGameScore(playerVector.at(0), *hand1);
+    int* sum2 = gameScore.computeGameScore(playerVector.at(1), *hand2);
+    int* sum3 = gameScore.computeGameScore(playerVector.at(2), *hand3);
+    cout << "Game Score for player" << playerVector.at(0)->getName() <<" is " << *sum1 << " points" << endl;
+    cout << "Game Score for player" << playerVector.at(1)->getName() <<" is " << *sum2 << " points" << endl;
+    cout << "Game Score for player" << playerVector.at(2)->getName() <<" is " << *sum3 << " points" << endl;
+    gameScore.winnerGenerator(playerVector);
+
+//    Player *ss = new Player("ss", 3, 15);
+//    Player *mm = new Player("mm", 3, 16);
+//    Player *jj = new Player("jj", 3, 17);
+//    Deck deck1;
+//    std::vector<Cards*> tb = deck1.topBoardGenetor(deck1);
+//    std::vector<Cards*> *hand = new std::vector<Cards*>();
+//    deck1.exchange(*ss, tb, deck1);
+//    deck1.exchange(*mm, tb, deck1);
+//    deck1.exchange(*jj, tb, deck1);
+//    deck1.exchange(*ss, tb, deck1);
+//    deck1.exchange(*mm, tb, deck1);
+//    deck1.exchange(*jj, tb, deck1);
+//    deck1.exchange(*ss, tb, deck1);
+//    deck1.exchange(*mm, tb, deck1);
+//    deck1.exchange(*jj, tb, deck1);
+//    deck1.exchange(*ss, tb, deck1);
+//    deck1.exchange(*mm, tb, deck1);
+//    deck1.exchange(*jj, tb, deck1);
+//    deck1.displayTopBoard(tb);
+//    deck1.displayHand(*ss->getGameHand());
+//    deck1.displayHand(*mm->getGameHand());
+//    deck1.displayHand(*jj->getGameHand());
+//    cout << "=========Test GameScore( ) ===============" << endl;
+//    std::vector<Player*> *players = new vector<Player*> ();
+//    GameScore gameScore;
+//    int* sum = gameScore.computeGameScore(*players, *ss->getGameHand());
+//    cout << "Game Score for player" << ss->getName() <<" is " << *sum << " points" << endl;
+}
