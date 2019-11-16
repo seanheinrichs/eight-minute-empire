a63b59311ffb56c78f227809e6bfbad1116056e1
cookfisher <cookfisher@outlook.com>
2019-11-16 14:47:49 -0500

    test master

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,14 @@
 #include "Map/MapDriver.h"
 #include "PlayerStrategies/PlayerStrategiesDriver.h"
+#include "Map/Map.h"
 
 int main()
 {
     //// Assignment 3 Drivers ////
 
     // Part 1)
-    playerStrategiesDriver();
+//    playerStrategiesDriver();
+//    Map::resetMapInstance();
 
     // Part 2)
 
