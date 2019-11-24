162db10dfe431f89f503602a2b5d2a264d89c0f9
cookfisher <cookfisher@outlook.com>
2019-11-23 20:49:10 -0500

    part 1 cardfactory

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,14 @@
 #include "PlayerStrategies/PlayerStrategiesDriver.h"
 #include "Map/Map.h"
 #include "GameEngine/GameEngineDriver.h"
+#include "CardsFactoryDriver.h"
 
 int main()
 {
     //// Assignment 4 Drivers ////
 
     // Part 1)
+//    cardsFactoryExample();
 
     // Part 2)
     gameEngineDriver();
